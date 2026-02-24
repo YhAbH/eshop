672e41d669e240f07a8ce33a37c0ee369097bdda
YhAbH <yahir.abraham.h@gmail.com>
2026-02-24 17:05:46 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import Stripe from "stripe";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(4242, () => console.log("Servidor activo"));
